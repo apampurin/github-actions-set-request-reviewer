d084910611f4bfc674694206ebdc1a68f60a2dd8
a.pampurin <a.pampurin@wellyes.ru>
2021-09-07 16:15:13 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,10 +18,10 @@ addInitialReviewers(){
 
 addFinalBOSS(){
   BOSS=$INPUT_FINAL_REVIEW
-  PULL_REQUEST_NUMBER=$(jq -r '.number' < "$GITHUB_EVENT_PATH")
+  PULL_REQUEST_API_URL=$(jq -r '.pull_request._links.self.href' < "$GITHUB_EVENT_PATH")
   cat $GITHUB_EVENT_PATH
-  curl -H "Authorization:token $INPUT_GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers"
-  CURRENT_REVIEWERS=$(curl -H "Authorization:token $INPUT_GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers" | jq -r '.users | .[].login')
+  curl -H "Authorization:token $INPUT_GITHUB_TOKEN" "$PULL_REQUEST_API_URL/requested_reviewers"
+  CURRENT_REVIEWERS=$(curl -H "Authorization:token $INPUT_GITHUB_TOKEN" "$PULL_REQUEST_API_URL/requested_reviewers" | jq -r '.users | .[].login')
   REVIEWERS=""
   for _REVIEWERS in ${CURRENT_REVIEWERS[@]}; do
     if [ $REVIEWERS == "" ]; then
