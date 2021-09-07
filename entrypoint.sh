2d2c90b693d560de3ca2839dd88c5a7ad9038bec
a.pampurin <a.pampurin@wellyes.ru>
2021-09-07 14:04:10 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ addFinalBOSS(){
   BOSS=$INPUT_FINAL_REVIEW
   PULL_REQUEST_NUMBER=$(jq -r '.number' < "$GITHUB_EVENT_PATH")
   CURRENT_REVIEWERS=$(curl -H "Authorization:token $INPUT_GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers | jq -r '.users | .[].login')
-  REVIEWERS+=("$CURRENT_REVIEWERS")
+  REVIEWERS="$CURRENT_REVIEWERS"
   REVIEWERS+=("$BOSS")
   ENDPOINT="https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers"
   CONTENTS="{\"reviewers\": $REVIEWERS, \"team_reviewers\": []}"
