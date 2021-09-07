896e40e08a094157efc7567497e7ca37f40b7580
a.pampurin <a.pampurin@wellyes.ru>
2021-09-07 14:17:19 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,17 +13,17 @@ addInitialReviewers(){
   curl -s -X POST -u "Authorization:token $INPUT_GITHUB_TOKEN" -H "$HEADER" -d "$CONTENTS" "$ENDPOINT"
 }
 
-addFinalBOSS(){
-  BOSS=$INPUT_FINAL_REVIEW
-  PULL_REQUEST_NUMBER=$(jq -r '.number' < "$GITHUB_EVENT_PATH")
-  CURRENT_REVIEWERS=$(curl -H "Authorization:token $INPUT_GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers | jq -r '.users | .[].login')
-  REVIEWERS="$CURRENT_REVIEWERS"
-  REVIEWERS+=("$BOSS")
-  ENDPOINT="https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers"
-  CONTENTS="{\"reviewers\": $REVIEWERS, \"team_reviewers\": []}"
-  HEADER="Accept: application/vnd.github.inertia-preview+json"
-  curl -s -X POST -H "Authorization:token $INPUT_GITHUB_TOKEN" -H "$HEADER" -d "$CONTENTS" "$ENDPOINT"
-}
+# addFinalBOSS(){
+#   BOSS=$INPUT_FINAL_REVIEW
+#   PULL_REQUEST_NUMBER=$(jq -r '.number' < "$GITHUB_EVENT_PATH")
+#   CURRENT_REVIEWERS=$(curl -H "Authorization:token $INPUT_GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers | jq -r '.users | .[].login')
+#   REVIEWERS="$CURRENT_REVIEWERS"
+#   REVIEWERS+=("$BOSS")
+#   ENDPOINT="https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers"
+#   CONTENTS="{\"reviewers\": $REVIEWERS, \"team_reviewers\": []}"
+#   HEADER="Accept: application/vnd.github.inertia-preview+json"
+#   curl -s -X POST -H "Authorization:token $INPUT_GITHUB_TOKEN" -H "$HEADER" -d "$CONTENTS" "$ENDPOINT"
+# }
 
 set_reviewers() {
   _CANDIDARES="$1"
@@ -49,8 +49,7 @@ set_reviewers() {
 
 if [ "$ACTION" == opened ]; then
   addInitialReviewers
-else
-  if [ -n $INPUT_FINAL_REVIEW ]; then
+fi
+if [ -n $INPUT_FINAL_REVIEW ]; then
     addFinalBOSS
-  fi
 fi
