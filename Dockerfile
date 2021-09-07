c72367265929b231d12a89a7124cd0fa6846878c
a.pampurin <a.pampurin@wellyes.ru>
2021-09-07 13:35:48 +0300

    initial rewrite

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,7 @@
-FROM alpine:3.9
+FROM alpine:3.10
 
-LABEL "com.github.actions.name"="Set request reviewer"
-LABEL "com.github.actions.description"="pull request add to reviewer"
-LABEL "com.github.actions.icon"="users"
-LABEL "com.github.actions.color"="orange"
+RUN apk add curl bash jq
+RUN apk add --update coreutils && rm -rf /var/cache/apk/*
 
-LABEL "repository"="https://github.com/usayuki/github-actions-set-request-reviewer"
-LABEL "maintainer"="usayuki <masapp.01a@gmail.com>"
-
-COPY entrypoint.sh /entrypoint.sh
+ADD entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,46 @@
 #!/bin/sh -eu
 
 ACTION=$(jq -r '.action' < "$GITHUB_EVENT_PATH")
+$INPUT_GITHUB_TOKEN
 
-if [ "$ACTION" != opened ]; then
-  echo "This action was ignored. (ACTION: $ACTION)"
-  exit 0
-fi
+addInitialReviewers(){
+  AUTHOR=$(jq -r '.pull_request.user.login' < "$GITHUB_EVENT_PATH")
+  REVIEWERS=$(set_reviewers "$INPUT_REVIEWERS" "$AUTHOR" "$INPUT_NUMBER_OF")
+  PULL_REQUEST_NUMBER=$(jq -r '.number' < "$GITHUB_EVENT_PATH")
 
-get_reviewers() {
+  ENDPOINT="https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers"
+  CONTENTS="{\"reviewers\": $REVIEWERS, \"team_reviewers\": []}"
+  HEADER="Accept: application/vnd.github.inertia-preview+json"
+  curl -s -X POST -u "Authorization:token $INPUT_GITHUB_TOKEN" -H "$HEADER" -d "$CONTENTS" "$ENDPOINT"
+}
+
+addFinalBOSS(){
+  BOSS=$INPUT_FINAL_REVIEW
+  PULL_REQUEST_NUMBER=$(jq -r '.number' < "$GITHUB_EVENT_PATH")
+  CURRENT_REVIEWERS=$(curl -H "Authorization:token $INPUT_GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers | jq -r '.users | .[].login')
+  REVIEWERS+=("$CURRENT_REVIEWERS")
+  REVIEWERS+=("$BOSS")
+  ENDPOINT="https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_REQUEST_NUMBER/requested_reviewers"
+  CONTENTS="{\"reviewers\": $REVIEWERS, \"team_reviewers\": []}"
+  HEADER="Accept: application/vnd.github.inertia-preview+json"
+  curl -s -X POST -H "Authorization:token $INPUT_GITHUB_TOKEN" -H "$HEADER" -d "$CONTENTS" "$ENDPOINT"
+}
+
+set_reviewers() {
   _CANDIDARES="$1"
   _AUTHOR="$2"
+  _NUMBER="$3"
   _REVIEWERS=()
   
+  echo $_AUTHOR
+  i=0
   for _CANDIDARE in ${$_CANDIDARES[@]}; do
     if [ "$_CANDIDARE" != "$_AUTHOR" ]; then
         _REVIEWERS+=("$_CANDIDARE")
+    fi
+    i=$((i++))
+    if [[ $i == 2 ]]; then
+      break
     fi
   done
 
@@ -22,15 +48,10 @@ get_reviewers() {
   unset _AUTHOR _REVIEWERS
 }
 
-AUTHOR=$(jq -r '.pull_request.user.login' < "$GITHUB_EVENT_PATH")
-REVIEWERS=$(get_reviewers "$*" "$AUTHOR")
-
-TOKEN="$GITHUB_TOKEN"
-
-PULL_REQUEST_NUMBER=$(jq -r '.number' < "$GITHUB_EVENT_PATH")
-
-ENDPOINT="https://api.github.com/repos/cam-inc/leahi-ios/pulls/$PULL_REQUEST_NUMBER/requested_reviewers"
-CONTENTS="{\"reviewers\": $REVIEWERS, \"team_reviewers\": []}"
-HEADER="Accept: application/vnd.github.inertia-preview+json"
-curl -s -X POST -u "$GITHUB_ACTOR:$TOKEN" -H "$HEADER" -d "$CONTENTS" "$ENDPOINT"
-
+if [ "$ACTION" == opened ]; then
+  addInitialReviewers
+else
+  if [ -n $INPUT_FINAL_REVIEW ]; then
+    addFinalBOSS
+  fi
+fi
